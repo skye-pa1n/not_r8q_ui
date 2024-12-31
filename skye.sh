69007e47d3774887c6cf036c3990d0237279a628
skye-pa1n <sonicgames772@gmail.com>
2024-12-31 07:22:37 -0500

    Misc dtb updates

--- skye.sh
+++ skye.sh
@@ -19,7 +19,7 @@ make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
 make -j10 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV dtbs
 
 DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > AnyKernel3/840dtb
+cat $DTB_OUT/*.dtb > AnyKernel3/a14.dtb
 
 make -j$(nproc --all) O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
 IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
