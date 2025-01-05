b82c3aad9c890c91705904cc62114ee293db00e2
skye-pa1n <sonicgames772@gmail.com>
2025-01-05 10:07:27 -0500

    kernel: sched: Tune latency

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -132,17 +132,28 @@ DEFINE_PER_CPU(int, fps_group_id);
  *   SCHED_TUNABLESCALING_LOG - scaled logarithmical, *1+ilog(ncpus)
  *   SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  *
- * (default SCHED_TUNABLESCALING_LOG = *(1+ilog(ncpus))
+ * (BORE default SCHED_TUNABLESCALING_NONE = *1 constant)
+ * (CFS  default SCHED_TUNABLESCALING_LOG  = *(1+ilog(ncpus))
  */
+#ifdef CONFIG_SCHED_BORE
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
+#else // CONFIG_SCHED_BORE
 enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
+#endif // CONFIG_SCHED_BORE
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
+ * (BORE default: 24ms constant, units: nanoseconds)
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
+#ifdef CONFIG_SCHED_BORE
+unsigned int sysctl_sched_latency			= 24000000ULL;
+static unsigned int normalized_sysctl_sched_latency	= 24000000ULL;
+#else // CONFIG_SCHED_BORE
 unsigned int sysctl_sched_base_slice			= 750000ULL;
 unsigned int normalized_sysctl_sched_base_slice		= 750000ULL;
+#endif // CONFIG_SCHED_BORE
 
 unsigned int __read_mostly sysctl_sched_migration_cost	= 250000UL; //1000000UL;
 #ifdef CONFIG_SCHED_BORE
