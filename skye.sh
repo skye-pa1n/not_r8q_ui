551707f6dc298235f1ada551cb22e2c9e5f1e153
skye-pa1n <sonicgames772@gmail.com>
2025-02-25 17:21:23 -0300

    ARM64: configs: "debloat"
    idk, i just dont like seeing 15+ dtbs doing nothing lol

--- skye.sh
+++ skye.sh
@@ -14,15 +14,17 @@ rm -rf /home/skye/bomb/AnyKernel3/dtb
 rm -rf .version
 rm -rf .local
 make O=/home/skye/bomb/out clean
-make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
+make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV dtb_defconfig
 
 make -j12 O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV dtbs
 DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
 cat $DTB_OUT/*.dtb > /home/skye/bomb/AnyKernel3/oneui.dtb
 
+make O=/home/skye/bomb/out clean
+make O=/home/skye/bomb/out ARCH=arm64 $BUILD_ENV not_defconfig
 make -j12 O=/home/skye/bomb/out ARCH=arm64 $KERNEL_MAKE_ENV $BUILD_ENV Image
 IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
-cp $IMAGE /home/skye/bomb/AnyKernel3/AOSP
+cp $IMAGE /home/skye/bomb/AnyKernel3/OneUI
 
 cd /home/skye/bomb/AnyKernel3
 rm *.zip
